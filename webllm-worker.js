23a46f63506727222f45827be5df152f3b1a8742
testeste55555 <258153139+testeste55555@users.noreply.github.com>
2026-08-25 21:02:46 +0900

    Remove local WebLLM worker

--- webllm-worker.js
+++
@@ -1,4 +0,0 @@
-import { WebWorkerMLCEngineHandler } from 'https://esm.run/@mlc-ai/web-llm@0.2.82';
-
-const handler = new WebWorkerMLCEngineHandler();
-self.onmessage = (event) => handler.onmessage(event);
